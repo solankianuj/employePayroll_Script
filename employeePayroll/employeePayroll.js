7683b26e8e8b8253af0b584177ecd8ccfab13ac4
solanki <anujaj2222.gmail.com>
2022-07-31 23:15:55 +0530

    Abitity to reset the form on clicking reset

--- employeePayroll/employeePayroll.js
+++ employeePayroll/employeePayroll.js
@@ -81,3 +81,31 @@ function createAndUpdateStorage(employeePayrollData){
     localStorage.setItem("EmployeePayrollList",JSON.stringify(employeePayrollList))
 }
 
+const resetForm =() =>{
+    setValue('#name', '');
+    insetSelectedValues('[name=profile]');
+    insetSelectedValues('[name=gender]');
+    insetSelectedValues('[name=department]');
+    setValue('#salary','');
+    setValue('#notes','');
+    setValue('#day','1');
+    setValue('#month','January');
+    setValue('#year','2020');
+}
+
+const unsetSelectedValues=(propertyValue) => {
+    let allItems =document.querySelectorAll(propertyValue);
+    allItems.forEach( item =>{
+        item.checked=false;
+    });
+}
+
+const setTextValue =(id,value) => {
+    const element=document.querySelector(id);
+    element.textContent=value;
+}
+
+const setValue =( id,value) => {
+    const element = document.querySelector(id);
+    element.value=value;
+}
